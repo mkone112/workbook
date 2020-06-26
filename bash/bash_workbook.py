794ef26aef6993e04380716b918321eee20a4f0d
Maxim Kalita <mkone112@gmail.com>
2020-06-26 07:05:44 +0300

    move bash notes from dj_girls

--- bash/bash_workbook.py
+++ bash/bash_workbook.py
@@ -1,2 +1,23 @@
 походу команды можно объединять
 	docker run -i -t	>> docker run -it
+	
+whoami  >> user name
+pwd
+#print working directory
+#работает в ps
+cd
+#Без параметров ~ pwd
+#change directory
+rm
+#
+    #удалить файл
+        rm
+    #удалить каталог
+        rm -r
+mv
+#перемещение файлов
+    mv <currentPath> <newPath>
+mkdir
+#Unix|Win
+cp
+#копирование файлов Unix
