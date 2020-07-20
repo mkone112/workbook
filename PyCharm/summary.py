40183db2b0e68eb3df35b622df4fda74fa493a1a
Maxim Kalita <mkone112@gmail.com>
2020-07-21 01:56:05 +0300

    several updates

--- PyCharm/summary.py
+++ PyCharm/summary.py
@@ -4,3 +4,16 @@ ctrl-alt-r
 #tools-> run manage.py task
 можно просматривать фаилы бд просто открыв их в проекте
 при переключении acejump на qwerty -> restart pycharm
+
+editor
+    ->general
+        caret movement
+            when moving by words -> stop at both word boundaries
+    ->autoimport
+        python
+            show import popup
+    ->code folding
+        collapse by default
+            file headers
+            imports
+colorsheme -> diff & merge отвечает за цвета рядом со scrollbars
