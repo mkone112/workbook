6e5069da3a434f1fa07ae7f2ace09ceec09e3271
mkone112 <mkone112@gmail.com>
2020-10-14 01:38:02 +0300

    Continue splitting PostgreSQL, update tasks, add Notebooks from notebook

--- Interview/tasks.py
+++ Interview/tasks.py
@@ -1,13 +1,20 @@
 asyncio
     aiohttp
     gevent
+        requests
 рефакторинг
     ветвление
         карта карно
 os
 процессы/потоки
 переменные окружения
-имя текущей fx
 абстрактные [базовые] классы
-bound method
+__all__
+cross join vs inner join vs join
 monkey patching
+как писать файлы на диск надежно?
+#временный файл?
+outer join
+#на примере
+как разбираться в структуре больших бд
+#проверить aviacompany в datagrip
--- Interview/tasks_old.py
+++ Interview/tasks_old.py
@@ -1,12 +1,27 @@
 __new__
+
 дескрипторы
 
 метаклассы 3.X
     typ или mcls
     #стандартное имя для первого arg в __new__ метакласса
+
 super
 
 __class__
+
 mro
 #порядок разрешения методов? не attr?
-процессы/потоки
+
+__dict__
+
+имя текущей fx
+    import inspect
+    def foo():
+        print( inspect.stack()[0][3] )
+    foo()
+
+bound method
+
+
+__slots__
