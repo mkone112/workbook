7b71d68e2ef8007e32473e6100607db37136aebd
Maxim Kalita <kalitamaxwork@gmail.com>
2021-10-09 18:44:58 +0300

    windows regedit

---
+++ Windows/registry_keys.py
@@ -0,0 +1,4 @@
+HKCU/Console/<path_to_powershell>/[ScreenColors]
+
+HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetReleaseVersion=1
+HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetReleaseVersionInfo=<winver>
